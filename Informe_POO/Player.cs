a6dfc7d1fb7f407acb621cff07c28810f57caadc
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-09-21 16:39:17 -0500

    Punto 5: Se creó la clase "Player", con sus respectivos metodos y atributos.

---
+++ Informe_POO/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Informe_POO
+{
+    class Player
+    {
+        private List<string> playerHand = new List<string>();
+
+        private List<string> AddCard(string card)
+        {
+            playerHand.Add(card);
+
+            return playerHand;
+        }
+
+        public List<string> Init(List<string> deck)
+        {
+            int deckSize = deck.Count;
+            int lastCard = deckSize - 1;
+
+            int condicionVariable = 52; // SI EL TALLADOR COMIENZA PRIMERO, SE DEBE CAMBIAR ESTE VALOR POR 48.
+
+            if (deckSize < condicionVariable && deckSize == 0)
+                throw new System.ArgumentOutOfRangeException("Error, please, generate a deck before, using this method.");
+            else
+            {
+                string card1 = deck[lastCard];
+                deck.Remove(card1);
+
+                string card2 = deck[lastCard - 1];
+                deck.Remove(card2);
+
+                AddCard(card1);
+                AddCard(card2);
+            }
+
+            return playerHand;
+
+        }
+    }
+}
