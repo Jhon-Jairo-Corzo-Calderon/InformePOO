bc0779956e6960d7a4c3326297b50ebb83f72a29
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-09-21 16:29:18 -0500

    Punto 3: se creó el metodo "Randomize" y el atributo "hand".

--- Informe_POO/Dealer.cs
+++ Informe_POO/Dealer.cs
@@ -9,10 +9,12 @@ namespace Informe_POO
     {
         private List<string> deck = new List<string>();
 
-
+        private List<string> dealerHand = new List<string>();
 
         public List<string> Generate()
         {
+            deck.Clear();
+
             Card card = new Card();
 
             List<string> suit = card.GetSuit();
@@ -27,13 +29,36 @@ namespace Informe_POO
                 foreach (string x in suit)
                 {
                     string cardSS = i + x;
-                    this.deck.Add(cardSS);
+                    deck.Add(cardSS);
                 }
             }
-
-            return this.deck;
+            return deck;
         }
 
+        public List<string> Randomize(List<string> deckInp)
+        {
+            List<string> originalDeck = new List<string>();
+
+            originalDeck = deck.GetRange(0, 51);
+
+            if (deckInp.Count == 0 || deck != deckInp)
+                throw new System.InvalidOperationException("Error, please, generate a deck before, using this method.");
+            else
+            {
+                Random rdm = new Random();
+
+                for (int i = 0; i < 51; i++)
+                {
+                    string singleCard = originalDeck[i];
+                    int rdmNum = rdm.Next(51);
+                    deckInp.Remove(singleCard);
+                    deckInp.Insert(rdmNum, singleCard);
+                }
+
+                return deckInp;
+
+            }
+        }
 
     }
 }
